243900a0b1b93b31e46f8dc09e6804c2bfe12414
Beaty Maina <beatymaina14@gmail.com>
2023-09-26 18:11:32 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Your code here
+/*const dodger = document.getElementById("dodger");
+function moveDodgerLeft()
+{
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  }); */
+
+  const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  // Adjust the maximum right position based on your frame width
+  const maxWidth = 400; // You can change this value as needed
+
+  if (left < maxWidth - dodger.clientWidth) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
